16594c0555b2c1575045d94fcfa2b371708f1939
dasariapoorva1922-collab <dasariapoorva4@gmail.com>
2025-08-02 23:57:53 +0530

    Create Variables.py

---
+++ BeginnerLevel/Variables.py
@@ -0,0 +1,18 @@
+#1: Variables
+
+#1.Create a variable named pi and store the value 22/7 in it
+
+pi=22/7
+print(f"Value of {pi}")
+print(f"Data type of pi: {type(pi)}")#check data type
+
+#2.Try to create a variable called 'for' and assign it a value 4
+for_variable=4
+print(f"Value of for_variable: {for_variable}")
+
+#3.Calculate Simple Interest 
+Principal=1000 #Principal amount in dollers
+rate=5 #Rate of interest in percent
+time=3 #Time in years
+simple_Interest=(Principal*rate*time)/100
+print(f"Simple Interset: {simple_Interest}")
